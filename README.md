b90220ed2594909c77d32f3531fd6637151a160c
tlattimore <thomas@lattimore.me>
2015-10-15 11:25:38 -0400

    Hello, World!
    This is the initial public commit of Drupal Sass Breakpoints. The node
    package that allows you to easily share breakpoints between Drupal 8 and
    your Sass code.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Drupal Sass Breakpoints
+
+Share breakpoints between a Drupal 8 theme and Sass. All done by some eyeglass magic!
+
+
+### License
+
+[MIT](https://opensource.org/licenses/MIT)
--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -1,22 +1,47 @@
 "use strict";
 
 var theme_path = process.env.PWD;
-var path = require("path");
-var fs   = require("fs");
-var glob = require("glob");
-var yaml = require("js-yaml");
-var breakpoints = yaml.safeLoad(fs.readFileSync(theme_path + '/breathy.breakpoints.yml', 'utf8'));
-
+var path = require('path');
+var fs   = require('fs');
+var glob = require('glob');
+var yaml = require('js-yaml');
 var dir = fs.readdirSync(theme_path);
-
-var breakpointsFile = glob.sync("*.breakpoints.yml", {"cwd": theme_path});
-
+var breakpointsFile = glob.sync('*.breakpoints.yml', {'cwd': theme_path});
 var breakpoints = yaml.safeLoad(fs.readFileSync(breakpointsFile[0], 'utf8'));
-function getMediaQuery(label) {
+
+/**
+ * Custom Function to get the value of a media query
+ * from our .breakpoints.yml.
+ *
+ * @param {String} targetLabel
+ *   The human readable name of the 'label' key for the breakpoint.
+ * @param {String} targetGroup
+ *   The name prefixes the breakpoint group name. For example: if the breakpoint
+ *   was `foo.bar: { ...` then the group name is "foo".
+ */
+var getMediaQuery = function(targetLabel, targetGroup) {
+  // Loop over all our breakpoints.
   for(var breakpoint in breakpoints) {
+    // Get the contents of the current breakpoint.
     var currentBreakpoint = breakpoints[breakpoint];
 
-    if (currentBreakpoint.label === label) {
+    // If a the value of group is not empty.
+    if (targetGroup.length) {
+      // Get the current breakpoint group we are on.
+      var currentGroup = breakpoint.match(/^([\w\-]+)/);
+
+      // If the current group matches the target group
+      // then return the query.
+      if (currentGroup[0] === targetGroup) {
+        if (currentBreakpoint.label === targetLabel) {
+          return currentBreakpoint.mediaQuery;
+        }
+      }
+    }
+
+    // If no group is listed, just return the media query for
+    // the 1st item that matches our target label
+    else if (currentBreakpoint.label === targetLabel) {
       return currentBreakpoint.mediaQuery;
     }
   }
@@ -24,12 +49,21 @@ function getMediaQuery(label) {
 
 module.exports = function(eyeglass, sass) {
   return {
-    sassDir: path.join(__dirname, "sass"),
+    sassDir: path.join(__dirname, 'sass'),
     functions: {
-      "get-breakpoint($label)": function(label, done) {
-        var label = label.getValue();
+      "dsb($label, $group: '')": function(label, group, done) {
 
-        done(sass.types.String(getMediaQuery(label)));
+        var label = label.getValue();
+        var group = group.getValue();
+
+        var mediaQuery = getMediaQuery(label, group);
+        // If no media query is found then throw an error.
+        if (typeof mediaQuery !== 'undefined') {
+          done(sass.types.String(getMediaQuery(label, group)));
+        }
+        else {
+          throw 'Unable to find a breakpoint within the "' + group + '" group with a label of "' + label + '" in the file ' + breakpointsFile + '.';
+        }
       }
     }
   }
